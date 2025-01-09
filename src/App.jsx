d90577d1947a14b1323106e57236a97608ee799b
khali <khalifalo178@gmail.com>
2025-01-09 12:13:42 +0100

    feature user login

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
-import PageLayout from './layout/PageLayout'
-import Home from './pages/home/Home'
-import NotFound from './pages/notFound/NotFound'
+import PageLayout from "./layout/PageLayout";
+import Home from "./pages/home/Home";
+import NotFound from "./pages/notFound/NotFound";
+import Login from "./pages/login/Login";
 
 export default function App() {
-    return (
-        <>
-            <Routes>
-                <Route element={<PageLayout />}>
-                    <Route path="/" element={<Home />} />
-                </Route>
-                <Route path='*' element={<NotFound />} />
-            </Routes>
-        </>
-    )
+  return (
+    <>
+      <Routes>
+        <Route element={<PageLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 export default function Home() {
-    return (
-        <main className="flex justify-center items-center min-h-[calc(100dvh-64px)] w-full max-w-screen-2xl mx-auto md:p-8 p-4">
-            <h1 className="text-3xl font-medium text-white">Home</h1>
-        </main>
-    );
+  return (
+    <main className="flex justify-center items-center min-h-[calc(100dvh-64px)] w-full max-w-screen-2xl mx-auto md:p-8 p-4">
+      <h1 className="text-3xl font-medium text-white">Home</h1>
+    </main>
+  );
 }
